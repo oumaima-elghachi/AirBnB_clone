b22737f0507c242b555bc321c393f27b1327f3ff
oumaima <elghachiomaima1@gmail.com>
2023-12-07 10:12:30 +0100

    test1

--- console.py
+++ console.py
@@ -29,3 +29,22 @@ def parse(arg):
         retl = [i.strip(",") for i in lexer]
         retl.append(curly_braces.group())
         return retl
+
+
+class HBNBCommand(cmd.Cmd):
+    """Defines the HBnB command interpreter.
+
+    Attributes:
+        prompt (str): The command prompt.
+    """
+
+    prompt = "(hbnb) "
+    __classes = {
+        "BaseModel",
+        "User",
+        "State",
+        "City",
+        "Place",
+        "Amenity",
+        "Review"
+    }
